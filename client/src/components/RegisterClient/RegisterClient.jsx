5ad76476fed1decb6f211cc36eb52adf7fd8888b
KyerrreN <kyerrrenmgt@gmail.com>
2024-12-11 05:40:28 +0300

    feat: add formik to validate

--- client/src/components/RegisterClient/RegisterClient.jsx
+++ client/src/components/RegisterClient/RegisterClient.jsx
@@ -1,137 +1,55 @@
 import { Link, useNavigate } from "react-router-dom";
-import { TextField, MenuItem, Button } from "@mui/material";
+import { TextField, Button } from "@mui/material";
 import "../LoginComponent/LoginComponent.css";
-import Specs from "../../util/specs.json";
 import { useState } from "react";
 import axios from "axios";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 export default function RegisterClient() {
     const navigate = useNavigate();
-    // Validation states
     const apiUrl = process.env.REACT_APP_URL;
     const [requestError, setRequestError] = useState("");
 
-    const [usernameError, setUsernameError] = useState("");
-    const [passwordError, setPasswordError] = useState("");
-    const [nameError, setNameError] = useState("");
-    const [surnameError, setSurnameError] = useState("");
-    const [emailError, setEmailError] = useState("");
-    const regexOneEnglishWord = /^[a-zA-Z]*$/;
-    const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    // Validation Schema
+    const validationSchema = Yup.object().shape({
+        username: Yup.string()
+            .min(8, "Username must contain at least 8 characters")
+            .matches(
+                /^[a-zA-Z0-9]+$/,
+                "Username can only be 1 English word with digits"
+            )
+            .required("Username is required"),
+        password: Yup.string()
+            .min(8, "Password must contain at least 8 characters")
+            .required("Password is required"),
+        name: Yup.string()
+            .max(20, "Name cannot exceed 20 characters")
+            .matches(/^[a-zA-Z]*$/, "Name can only be 1 English word")
+            .required("Name is required"),
+        surname: Yup.string()
+            .max(20, "Surname cannot exceed 20 characters")
+            .matches(/^[a-zA-Z]*$/, "Surname can only be 1 English word")
+            .required("Surname is required"),
+        email: Yup.string()
+            .email("Invalid email format")
+            .max(80, "Email cannot exceed 80 characters")
+            .required("E-mail is required"),
+    });
 
-    const handleUsernameChange = (e) => {
-        const usernameValue = e.target.value.trim();
-
-        if (usernameValue.length < 8) {
-            setUsernameError("Username must containt atleast 8 characters");
-        } else {
-            setUsernameError("");
+    const handleSubmit = async (values, { setSubmitting }) => {
+        try {
+            await axios.post(`${apiUrl}/auth/register/client`, values);
+            setRequestError("");
+            navigate("/");
+        } catch (e) {
+            setRequestError(`Error: ${e.response?.data?.message}`);
+            console.log(e.response?.data?.message);
+        } finally {
+            setSubmitting(false);
         }
     };
 
-    const handlePasswordChange = (e) => {
-        const passwordValue = e.target.value.trim();
-
-        if (!passwordValue) {
-            setPasswordError("Password is required");
-        } else if (passwordValue.length < 8) {
-            setPasswordError("Password must contain atleast 8 characters");
-        } else {
-            setPasswordError("");
-        }
-    };
-
-    const handleNameChange = (e) => {
-        const nameValue = e.target.value.trim();
-
-        if (nameValue.length > 20) {
-            setNameError("Name cannot exceed 20 characters");
-            return;
-        }
-
-        if (!regexOneEnglishWord.test(nameValue)) {
-            setNameError("Name can only be 1 english word");
-            return;
-        }
-
-        setNameError("");
-    };
-
-    const handleSurnameChange = (e) => {
-        const surnameValue = e.target.value.trim();
-
-        if (surnameValue.length > 20) {
-            setSurnameError("Surname cannot exceed 20 characters");
-            return;
-        }
-
-        if (!regexOneEnglishWord.test(surnameValue)) {
-            setSurnameError("Surname can only be 1 english word");
-            return;
-        }
-
-        setSurnameError("");
-    };
-
-    const handleEmailChange = (e) => {
-        const emailValue = e.target.value.trim();
-
-        if (emailValue.length > 80) {
-            setEmailError("Email cannot exceed 80 characters");
-            return;
-        }
-
-        if (!regexEmail.test(emailValue)) {
-            setEmailError(
-                "Doesn't match real email. Example: example@gmail.com"
-            );
-            return;
-        }
-
-        setEmailError("");
-    };
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-
-        if (
-            !Boolean(usernameError) &&
-            !Boolean(passwordError) &&
-            !Boolean(nameError) &&
-            !Boolean(surnameError) &&
-            !Boolean(emailError)
-        ) {
-            const formData = new FormData(e.currentTarget);
-            const formJson = Object.fromEntries(formData.entries());
-            console.log({
-                name: formJson.name,
-                surname: formJson.surname,
-                email: formJson.email,
-                username: formJson.username,
-                password: formJson.password,
-            });
-
-            try {
-                const response = await axios.post(
-                    `${apiUrl}/auth/register/client`,
-                    {
-                        name: formJson.name,
-                        surname: formJson.surname,
-                        email: formJson.email,
-                        username: formJson.username,
-                        password: formJson.password,
-                    }
-                );
-
-                setRequestError("");
-
-                return navigate("/");
-            } catch (e) {
-                setRequestError(`Error: ${e.response?.data?.message}`);
-                console.log(e.response?.data?.message);
-            }
-        }
-    };
     return (
         <div className="container login-frame">
             <div>
@@ -140,89 +58,121 @@ export default function RegisterClient() {
             </div>
             <h1>Register as a Client</h1>
 
-            <form className="login-form" onSubmit={handleSubmit}>
-                <h2>Login information</h2>
-                <TextField
-                    required
-                    id="username"
-                    name="username"
-                    label="Username"
-                    type="text"
-                    fullWidth
-                    margin="dense"
-                    onChange={handleUsernameChange}
-                    error={Boolean(usernameError)}
-                    helperText={usernameError}
-                    InputLabelProps={{ shrink: true }}
-                />
+            <Formik
+                initialValues={{
+                    username: "",
+                    password: "",
+                    name: "",
+                    surname: "",
+                    email: "",
+                }}
+                validationSchema={validationSchema}
+                onSubmit={handleSubmit}
+            >
+                {({ isSubmitting }) => (
+                    <Form className="login-form">
+                        <h2>Login information</h2>
+                        <Field name="username">
+                            {({ field, meta }) => (
+                                <TextField
+                                    {...field}
+                                    required
+                                    id="username"
+                                    label="Username"
+                                    type="text"
+                                    fullWidth
+                                    margin="dense"
+                                    InputLabelProps={{ shrink: true }}
+                                    error={meta.touched && Boolean(meta.error)}
+                                    helperText={meta.touched && meta.error}
+                                />
+                            )}
+                        </Field>
 
-                <TextField
-                    required
-                    id="password"
-                    name="password"
-                    label="Password"
-                    type="password"
-                    fullWidth
-                    margin="dense"
-                    onChange={handlePasswordChange}
-                    error={Boolean(passwordError)}
-                    helperText={passwordError}
-                    InputLabelProps={{ shrink: true }}
-                />
+                        <Field name="password">
+                            {({ field, meta }) => (
+                                <TextField
+                                    {...field}
+                                    required
+                                    id="password"
+                                    label="Password"
+                                    type="password"
+                                    fullWidth
+                                    margin="dense"
+                                    InputLabelProps={{ shrink: true }}
+                                    error={meta.touched && Boolean(meta.error)}
+                                    helperText={meta.touched && meta.error}
+                                />
+                            )}
+                        </Field>
 
-                <h2>Credentials</h2>
+                        <h2>Credentials</h2>
 
-                <TextField
-                    required
-                    id="name"
-                    name="name"
-                    label="Name"
-                    type="text"
-                    fullWidth
-                    margin="dense"
-                    onChange={handleNameChange}
-                    error={Boolean(nameError)}
-                    helperText={nameError}
-                    InputLabelProps={{ shrink: true }}
-                />
+                        <Field name="name">
+                            {({ field, meta }) => (
+                                <TextField
+                                    {...field}
+                                    required
+                                    id="name"
+                                    label="Name"
+                                    type="text"
+                                    fullWidth
+                                    margin="dense"
+                                    InputLabelProps={{ shrink: true }}
+                                    error={meta.touched && Boolean(meta.error)}
+                                    helperText={meta.touched && meta.error}
+                                />
+                            )}
+                        </Field>
 
-                <TextField
-                    required
-                    id="surname"
-                    name="surname"
-                    label="Surname"
-                    type="text"
-                    fullWidth
-                    margin="dense"
-                    onChange={handleSurnameChange}
-                    error={Boolean(surnameError)}
-                    helperText={surnameError}
-                    InputLabelProps={{ shrink: true }}
-                />
+                        <Field name="surname">
+                            {({ field, meta }) => (
+                                <TextField
+                                    {...field}
+                                    required
+                                    id="surname"
+                                    label="Surname"
+                                    type="text"
+                                    fullWidth
+                                    margin="dense"
+                                    InputLabelProps={{ shrink: true }}
+                                    error={meta.touched && Boolean(meta.error)}
+                                    helperText={meta.touched && meta.error}
+                                />
+                            )}
+                        </Field>
 
-                <TextField
-                    required
-                    id="email"
-                    name="email"
-                    label="E-mail"
-                    type="text"
-                    fullWidth
-                    margin="dense"
-                    onChange={handleEmailChange}
-                    error={Boolean(emailError)}
-                    helperText={emailError}
-                    InputLabelProps={{ shrink: true }}
-                />
+                        <Field name="email">
+                            {({ field, meta }) => (
+                                <TextField
+                                    {...field}
+                                    required
+                                    id="email"
+                                    label="E-mail"
+                                    type="text"
+                                    fullWidth
+                                    margin="dense"
+                                    InputLabelProps={{ shrink: true }}
+                                    error={meta.touched && Boolean(meta.error)}
+                                    helperText={meta.touched && meta.error}
+                                />
+                            )}
+                        </Field>
 
-                <Button type="submit" variant="contained" color="secondary">
-                    Register
-                </Button>
-            </form>
+                        <Button
+                            type="submit"
+                            variant="contained"
+                            color="secondary"
+                            disabled={isSubmitting}
+                        >
+                            Register
+                        </Button>
+                    </Form>
+                )}
+            </Formik>
 
-            {Boolean(requestError) ? (
+            {Boolean(requestError) && (
                 <h1 style={{ color: "red" }}>{requestError}</h1>
-            ) : (
-                <></>
             )}
         </div>
     );
