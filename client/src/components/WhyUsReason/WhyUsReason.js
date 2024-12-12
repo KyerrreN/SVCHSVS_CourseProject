4eccf7b6dfe79be5210a27de73860b5a64d79092
KyerrreN <kyerrrenmgt@gmail.com>
2024-12-12 19:36:51 +0300

    fix: aos library made register pages unscrollable. remove aos

--- client/src/components/WhyUsReason/WhyUsReason.js
+++ client/src/components/WhyUsReason/WhyUsReason.js
@@ -4,12 +4,15 @@ import Aos from "aos";
 import "aos/dist/aos.css";
 
 export default function WhyUsReason({ image, alt, header, description }) {
-    useEffect(() => {
-        Aos.init({ duration: 1500 });
-    }, []);
+    // useEffect(() => {
+    //     Aos.init({ duration: 1500 });
+    // }, []);
 
     return (
-        <div className="whyus-reason" data-aos="zoom-in">
+        <div
+            className="whyus-reason"
+            // data-aos="zoom-in"
+        >
             <div className="whyus-reason-pic">
                 <img src={image} alt={alt}></img>
             </div>
